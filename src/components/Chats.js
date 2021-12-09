3dca92e66a3fb05180e669d99078e5168041c8c1
Mohit Gupta <mohitguptatech18@gmail.com>
2021-12-09 15:06:52 +0530

    chats component add fully

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,10 +1,28 @@
 import React from "react";
-
+import { useHistory } from "react-router-dom";
+import { ChatEngine } from "react-chat-engine";
+import { auth } from "./Firebase";
 const Chats = () => {
+  const history = useHistory();
+
+  const handleLogout = async () => {
+    await auth.signOut();
+    history.pushState("/");
+  };
+
   return (
-    <>
-      <h1>Chats</h1>
-    </>
+    <div className="chats-page">
+      <div className="nav-bar">
+        <div className="logo-tab">LetsChat</div>
+        <div onClick={handleLogout} className="logout-tab">
+          Logout
+        </div>
+      </div>
+      <ChatEngine
+        height="Calc(100vh-66px)"
+        projectId="5497b895-e1e4-4ce0-a7e1-613569648511"
+      />
+    </div>
   );
 };
 
